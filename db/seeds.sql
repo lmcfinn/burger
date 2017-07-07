383f0190c3cd0d8d3bf35f6a6264e8f8abcce9f3
Shane <lmcfinn@yahoo.com>
2017-07-06 20:43:37 -0500

    seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,9 @@ INSERT INTO burgers (burger_name) VALUES ("Fries Burger");
 
 UPDATE burgers SET devoured = false WHERE id = 7;
 DELETE FROM burgers WHERE id=12;
+
+
+INSERT INTO burgers (burger_name, devoured, createdAt) VALUES (“Duck Breast Burger”, true, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, createdAt) VALUES (“Bacon Burger”, true, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, createdAt) VALUES (“Veggie Burger”, false, CURRENT_TIMESTAMP);
+INSERT INTO burgers (burger_name, devoured, createdAt) VALUES (“Fries Burger”, false, CURRENT_TIMESTAMP);
